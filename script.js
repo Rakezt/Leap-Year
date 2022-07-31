9b75081919a73f08fccfb73a67ce81e3928baff8
Rakezt <keztmckinnon@gmail.com>
2022-07-31 03:48:41 -0400

    code update

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ var userName = readlineSync.question("Naam dedo bhaiya.....")
 var potato = "potato"
 
 console.log("Welcome " + (userName) + ", to the bandhar club");
-console.log(chalk.red("aayush"))
 
 var userDOB = readlineSync.question("please input year of birth plaese.....")
 
 if ((userDOB % 4 === 0) && (userDOB % 100 === !0) || (userDOB % 400 === 0)) {
   console.log(chalk.greenBright("You are born on a leap year"));
+} else if (userDOB === "potato") {
+  console.log(chalk.redBright("You are Fake"))
 } else {
 
-  console.log(chalk.red("You are not born on a leap year"))
+  console.log(chalk.bgBlueBright("You are not born on a leap year"))
 }
 
