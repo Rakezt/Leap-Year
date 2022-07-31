cf7e8aa88df2941b777b14b5df4b3113f50cbd4b
Rakezt <keztmckinnon@gmail.com>
2022-07-31 03:39:39 -0400

    update script

---
+++ script.js
@@ -0,0 +1,17 @@
+var readlineSync = require('readline-sync')
+var chalk = require("chalk");
+var userName = readlineSync.question("Naam dedo bhaiya.....")
+var potato = "potato"
+
+console.log("Welcome " + (userName) + ", to the bandhar club");
+console.log(chalk.red("aayush"))
+
+var userDOB = readlineSync.question("please input year of birth plaese.....")
+
+if ((userDOB % 4 === 0) && (userDOB % 100 === !0) || (userDOB % 400 === 0)) {
+  console.log(chalk.greenBright("You are born on a leap year"));
+} else {
+
+  console.log(chalk.red("You are not born on a leap year"))
+}
+
